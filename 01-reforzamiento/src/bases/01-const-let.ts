ec2c826283555c87774bb9668182eca322f62982
Ana Rotela <anarotela18@gmail.com>
2025-09-13 18:02:57 -0300

    Clase 13: Variables y constantes

---
+++ 01-reforzamiento/src/bases/01-const-let.ts
@@ -0,0 +1,11 @@
+const firstName: string = 'Ana';
+let lastName = 'Rotela';
+
+let diceNumber = 5;
+diceNumber = 3;
+//console.log(firstName, lastName);
+
+const containsLetterH = lastName.includes('h');
+
+firstName
+console.log({containsLetterH, diceNumber, firstName, lastName});
